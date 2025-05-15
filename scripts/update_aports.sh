01150fd873d2915242da134c52995ed182aca622
Atsushi Watanabe <atsushi.w@ieee.org>
2025-05-15 15:36:40 +0900

    Format shell script

--- scripts/update_aports.sh
+++ scripts/update_aports.sh
@@ -13,7 +13,8 @@ aports_slug='seqsense/aports-ros-experimental'
 distro_dir=${ros_distro}
 case ${ros_distro} in
   "noetic")
-    distro_dir=${ros_distro}.v${alpine_version};;
+    distro_dir=${ros_distro}.v${alpine_version}
+    ;;
   *)
     echo "Unsupported ROS_DISTRO"
     ;;
@@ -24,16 +25,14 @@ case "${ALPINE_VERSION}" in
   3.20)
     generate_opts="${generate_opts} --split-dev"
     ;;
-  *)
-    ;;
+  *) ;;
 esac
 
 branch="rosdistro1-${distro_dir}-${short_hash}"
 
 if git ls-remote --exit-code \
   https://github.com/${aports_slug}.git \
-  refs/heads/${branch}
-then
+  refs/heads/${branch}; then
   echo "The change is already pushed"
   exit 0
 fi
@@ -43,7 +42,6 @@ fi
 rosdistro_build_cache index.yaml ${ros_distro}
 export ROSDISTRO_INDEX_URL="file://$(pwd)/index.yaml"
 rosdep update
-
 
 # Clone and update aports
 
@@ -57,25 +55,22 @@ cd ${tmpdir}/v${alpine_version}/ros/${ros_distro}
 git config user.name "at-wat"
 git config user.email "8390204+at-wat@users.noreply.github.com"
 
-
 generate-rospkg-apkbuild-multi --all ${ros_distro} ${generate_opts}
-
 
 git checkout -b ${branch}
 git add .
 
-if git diff --cached --exit-code
-then
+if git diff --cached --exit-code; then
   echo "No update found"
   exit 0
 fi
 
 git commit -m "Update ${distro_dir} aports (rosdistro ${short_hash})"
 
-
 # Push and open PR
 
-pr_request_body=$(cat << EOS
+pr_request_body=$(
+  cat <<EOS
 {
   "title": "Update ${distro_dir} aports (rosdistro ${short_hash})",
   "body": "Upstream commit messages\\n\`\`\`\\n${upstream_commit_msg}\\n\`\`\`",
@@ -86,20 +81,21 @@ EOS
 )
 echo ${pr_request_body}
 
-if ! ${DRYRUN:-true}
-then
-  if ! git push origin ${branch}
-  then
+if ! ${DRYRUN:-true}; then
+  if ! git push origin ${branch}; then
     if git ls-remote --exit-code \
       https://github.com/${aports_slug}.git \
-      refs/heads/${branch}
-    then
+      refs/heads/${branch}; then
       echo "The change is already pushed"
       exit 0
     fi
   fi
 
   curl https://api.github.com/repos/${aports_slug}/pulls -d "${pr_request_body}" -XPOST -n \
-    || (echo "Failed to open a pull request. GitHub personal access token for api.github.com is not set up."; \
-        echo "Please manually open the pull request."; echo; exit 1)
+    || (
+      echo "Failed to open a pull request. GitHub personal access token for api.github.com is not set up."
+      echo "Please manually open the pull request."
+      echo
+      exit 1
+    )
 fi
